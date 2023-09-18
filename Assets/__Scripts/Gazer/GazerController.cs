59a4adb21792ee681ba774dc1dc427b13798a7c8
Paul <pbochman@hs-mittweida.de>
2023-09-18 16:58:57 +0200

    added a lot of shit, dont know what exaclty tbh

--- Assets/__Scripts/Gazer/GazerController.cs
+++ Assets/__Scripts/Gazer/GazerController.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class GazerController : MonoBehaviour
 {
     
+    
     [SerializeField] private float _speed = 1f;
     [SerializeField] private float _growthRate = 1f;
+    [SerializeField] private float _minSize = 1f;
     [SerializeField] private float _maxSize = 1f;
     [SerializeField] private float _damage = 1f;
+    [SerializeField] private float _chargeSpeed = 1f;
+    [SerializeField] private float _chargeTime = 1f;
+    [SerializeField] private float _chargeCooldown = 1f;
+
+    
+
 
 
     private Rigidbody _rigidbody;
     private Transform _transform;
+    private float _startTime;
+    private bool _charging = false;
 
 
 
@@ -21,6 +31,7 @@ public class GazerController : MonoBehaviour
     {
         _rigidbody = GetComponent<Rigidbody>();
         _transform = GetComponent<Transform>();
+        _startTime = Time.time;
     }
 
 
@@ -37,10 +48,58 @@ public class GazerController : MonoBehaviour
             if (_transform.localScale.x < _maxSize) {
                 _transform.localScale *= _growthRate;
             }
+
+            //add +-10 degrees to current direction
+            Vector3 direction = _rigidbody.velocity;
+            direction = Quaternion.Euler(0, Random.Range(-10f,10f), 0) * direction;
+            direction.Normalize();
+            
+
+
+            if (direction.z < 0.1f && direction.z > 0f) {
+                direction.z = 0.1f;
+            }
+            else if (direction.z < 0f && direction.z > -0.1f) {
+                direction.z = -0.1f;
+            }
+
+            if (direction.x < 0.1f && direction.x > 0f) {
+                direction.x = 0.1f;
+            }
+            else if (direction.x < 0f && direction.x > -0.1f) {
+                direction.x = -0.1f;
+            }
+            _rigidbody.velocity = direction * _speed;
         }
         else if (other.gameObject.CompareTag("Player")) {
             //TODO: damage player
             //other.gameObject.GetComponent<PlayerController>().TakeDamage(_damage);
         }
     }
+
+    private void Update() {
+        if (Time.time - _startTime > _chargeCooldown && !_charging) {
+            _charging = true;
+            StartCoroutine(charge());
+        }
+    }
+
+
+    private IEnumerator charge() {
+        _rigidbody.velocity = Vector3.zero;
+        yield return new WaitForSeconds(_chargeTime);
+        
+        List<GameObject> players = new List<GameObject>(GameObject.FindGameObjectsWithTag("Player"));
+        GameObject player = players[Random.Range(0, players.Count)];
+
+        Vector3 direction = player.transform.position - _transform.position;
+        direction.y = 0;
+        direction.Normalize();
+        _rigidbody.velocity = direction * _chargeSpeed;
+        _startTime = Time.time;
+        _charging = false;
+    }
+
+
+    
 }
--- Assets/__Scripts/Manager/GameManager.cs
+++ Assets/__Scripts/Manager/GameManager.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    [Header("Phase Settings")]
+    [SerializeField] private float _timeBeforePhase1 = 1f;
+    [SerializeField] private float _phase1Duration = 1f;
+    [SerializeField] private float _gazerSizePhase1 = 1f;
+    [SerializeField] private float _timeBeforePhase2 = 1f;
+    [SerializeField] private float _phase2Duration = 1f;
+    [SerializeField] private float _gazerSizePhase2 = 1f;
+    [SerializeField] private GameObject _gazerPrefab;
+
+    [Header("UI Settings")]
+    [SerializeField] private GameObject _gameOverScreen;
+
+
+
+    public static GameManager instance = null;
+    private float _startTime;
+    private bool _spawnedGazer1 = false;
+    private bool _spawnedGazer2 = false;
+    private bool _gameOver = false;
+
+
+    private void Awake()
     {
-        
+        instance = this;
+        _startTime = Time.time;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    private void Update() {
+        if (Time.time - _startTime > _timeBeforePhase1 && Time.time - _startTime < _timeBeforePhase1 + _phase1Duration && !_spawnedGazer1) {
+            //phase 1
+            Debug.Log("Start phase 1");
+            GameObject obj = Instantiate(_gazerPrefab, new Vector3(0, 0.001f, 0), Quaternion.identity);
+            obj.transform.SetParent(transform);
+            obj.transform.localScale = new Vector3(_gazerSizePhase1, 0, _gazerSizePhase1);
+            _spawnedGazer1 = true;
+        }
+        else if (Time.time - _startTime > _timeBeforePhase1 + _phase1Duration + _timeBeforePhase2 && Time.time - _startTime < _timeBeforePhase1 + _phase1Duration + _timeBeforePhase2 + _phase2Duration && !_spawnedGazer2) {
+            //phase 2
+            Debug.Log("Start phase 2");
+            GameObject obj = Instantiate(_gazerPrefab, new Vector3(0, 0.001f, 0), Quaternion.identity);
+            obj.transform.SetParent(transform);
+            obj.transform.localScale = new Vector3(_gazerSizePhase2, 0, _gazerSizePhase2);
+            _spawnedGazer2 = true;
+        }
+        else if (Time.time - _startTime > _timeBeforePhase1 + _phase1Duration + _timeBeforePhase2 + _phase2Duration && !_gameOver) {
+            //game over
+            _gameOver = true;
+            //GameOver();
+        }
     }
+
+    public void GameOver() {
+        Time.timeScale = 0;
+        _gameOverScreen.SetActive(true);
+    }
+
+
+    public void Restart() {
+        Time.timeScale = 1;
+        _gameOverScreen.SetActive(false);
+        //get active scene
+        int currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(currentScene);
+    }
+
 }
