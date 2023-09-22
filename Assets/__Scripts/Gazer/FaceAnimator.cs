d85436011a8ef86a0ee4233c0e4136aeca84df7e
Paul <pbochman@hs-mittweida.de>
2023-09-22 19:55:08 +0200

    ...

---
+++ Assets/__Scripts/Gazer/FaceAnimator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceAnimator : MonoBehaviour
+{
+    
+
+    [SerializeField] List<Sprite> angryFaces;
+
+
+
+
+}
