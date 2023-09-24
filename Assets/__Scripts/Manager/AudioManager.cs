e6abf138109235353049ea8658672a6d33b6ad9a
Paul <pbochman@hs-mittweida.de>
2023-09-24 20:44:38 +0200

    added music to main menu

--- Assets/__Scripts/Manager/AudioManager.cs
+++ Assets/__Scripts/Manager/AudioManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.Audio;
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] private AudioMixerGroup _audioMixerGroup;
+    [SerializeField] private bool _isMainmenu;
     [SerializeField] private AudioClip _musicCut;
     [SerializeField] private AudioClip _musicLoop;
 
@@ -20,13 +21,18 @@ public class AudioManager : MonoBehaviour
         _musicAudioSource = GetComponent<AudioSource>();
         _musicAudioSource.clip = _musicCut;
         _musicAudioSource.outputAudioMixerGroup = _audioMixerGroup;
-        _musicAudioSource.loop = false;
+        if (_isMainmenu) {
+            _musicAudioSource.loop = true;
+        }
+        else {
+            _musicAudioSource.loop = false;
+        }
         _startTime = Time.time;
         _musicAudioSource.Play();
     }
 
     private void Update() {
-        if (!_isLooping && Time.time - _startTime > _musicCut.length) {
+        if (!_isLooping && Time.time - _startTime > _musicCut.length && !_isMainmenu) {
             _musicAudioSource.clip = _musicLoop;
             _musicAudioSource.loop = true;
             _musicAudioSource.Play();
